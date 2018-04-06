1d5ff620d65a8ffe956b4cb095a41a2018b111d0
pqdn13 <pqdn@yandex.ru>
2018-04-06 16:55:32 +0300

    chapter 1 - semaphore

---
+++ test/src/main/java/chapter3/lesson1/Job.java
@@ -0,0 +1,15 @@
+package chapter3.lesson1;
+
+public class Job implements Runnable {
+    private PrintQueue printQueue;
+
+    public Job(PrintQueue printQueue) {
+        this.printQueue = printQueue;
+    }
+
+    public void run() {
+        System.out.printf("%s: Going to print a job\n", Thread.currentThread().getName());
+        printQueue.printJob(new Object());
+        System.out.printf("%s: The document has been printed\n", Thread.currentThread().getName());
+    }
+}
---
+++ test/src/main/java/chapter3/lesson1/Main.java
@@ -0,0 +1,16 @@
+package chapter3.lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        PrintQueue printQueue = new PrintQueue();
+        Thread[] threads = new Thread[12];
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new Job(printQueue), "Thread" + i);
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].start();
+        }
+    }
+}
