3dad037b054dfaefeff5f51416f2ff826909fd10
pqdn13 <pqdn@yandex.ru>
2018-04-19 12:08:34 +0300

    chapter 1 - semaphore

---
+++ test/src/main/java/chapter3/lesson2/Videoconference.java
@@ -0,0 +1,28 @@
+package chapter3.lesson2;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Videoconference implements Runnable {
+    private final CountDownLatch controller;
+
+    public Videoconference(int number) {
+        controller = new CountDownLatch(number);
+    }
+
+    public void arrive(String name) {
+        System.out.printf("%s has arrived.", name);
+        controller.countDown();
+        System.out.printf("VideoConference: Waiting for %d participants.\n", controller.getCount());
+    }
+
+    public void run() {
+        System.out.printf("VideoConference: Initialization: %d participants.\n", controller.getCount());
+        try {
+            controller.await();
+            System.out.printf("VideoConference: All the participants have come\n");
+            System.out.printf("VideoConference: Let's start...\n");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
